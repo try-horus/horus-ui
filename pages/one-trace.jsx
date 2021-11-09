d4f716668bbc16bf679439d25bdb12e316f9dcef
Callie <48188690+callieburuchara@users.noreply.github.com>
2021-11-09 10:50:11 -0500

    Initial page three set up

---
+++ pages/one-trace.jsx
@@ -0,0 +1,20 @@
+function oneTrace() {
+  return (
+    <div className="m-3">
+    <header className="bg-blue-500 text-white p-10">
+      <img className="rounded-lg w-24" src="horus-eye.png"/>
+    </header>
+    <main className="p-5" height="1000px">
+      <div className="flex justify-end">
+
+      </div>
+      <div className="mt-5 justify-center">
+
+      </div>
+    </main>
+  </div>
+  )
+}
+
+
+export default oneTrace
