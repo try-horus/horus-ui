a2cf18c2c580e6f10537faa50964671dfc8713f1
richwynmorris <rwmorris.bjj@gmail.com>
2021-11-11 17:32:55 +0000

    added SQL statement to get latency data from db

--- server/utils/formatLatency.js
+++ server/utils/formatLatency.js
@@ -21,14 +21,54 @@ const formatLatencyQuery = ( timeframe ) => {
     SELECT time,
       (
         CASE
-          WHEN value >= lag(value) OVER w
-            THEN value - lag(value) OVER w
-          WHEN lag(value) OVER w IS NULL THEN NULL
-          ELSE value
+          WHEN bucket_500 >= lag(bucket_500) OVER w
+            THEN bucket_500 - lag(bucket_500) OVER w
+          WHEN lag(bucket_500) OVER w IS NULL THEN NULL
+          ELSE bucket_500
         END
-      ) AS "errors"
-      FROM eps
-      WHERE time > NOW() - INTERVAL '${queryTimeframe}'
+      ) AS "500",
+      (
+        CASE
+          WHEN bucket_1000 >= lag(bucket_1000) OVER w
+            THEN bucket_1000 - lag(bucket_1000) OVER w
+          WHEN lag(bucket_1000) OVER w IS NULL THEN NULL
+          ELSE bucket_1000
+        END
+      ) AS "1000",
+      (
+        CASE
+          WHEN bucket_1500 >= lag(bucket_1500) OVER w
+            THEN bucket_1500 - lag(bucket_1500) OVER w
+          WHEN lag(bucket_1500) OVER w IS NULL THEN NULL
+          ELSE bucket_1500
+        END
+      ) AS "1500",
+      (
+        CASE
+          WHEN bucket_2000 >= lag(bucket_2000) OVER w
+            THEN bucket_2000 - lag(bucket_2000) OVER w
+          WHEN lag(bucket_2000) OVER w IS NULL THEN NULL
+          ELSE bucket_2000
+        END
+      ) AS "2000",
+      (
+        CASE
+          WHEN bucket_2500 >= lag(bucket_2500) OVER w
+            THEN bucket_2500 - lag(bucket_2500) OVER w
+          WHEN lag(bucket_2500) OVER w IS NULL THEN NULL
+          ELSE bucket_2500
+        END
+      ) AS "2500",
+      (
+        CASE
+          WHEN bucket_over_2500 >= lag(bucket_over_2500) OVER w
+            THEN bucket_over_2500 - lag(bucket_over_2500) OVER w
+          WHEN lag(bucket_over_2500) OVER w IS NULL THEN NULL
+          ELSE bucket_over_2500
+        END
+      ) AS "2500+"
+      FROM latency
+      WHERE time > NOW() - INTERVAL '1 hour'
       WINDOW w AS (ORDER BY time)
       ORDER BY time;
   `
