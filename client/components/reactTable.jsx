377a9dc460683c773afd0f8c5c1959d220e8951d
Callie <48188690+callieburuchara@users.noreply.github.com>
2021-11-15 17:21:54 -0500

    Errors are truly showing on Page Two

--- client/components/reactTable.jsx
+++ client/components/reactTable.jsx
@@ -1,6 +1,7 @@
 import DataTable from 'react-data-table-component';
 import { useState } from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router'
 
 const columns = [
     {
@@ -36,54 +37,55 @@ const columns = [
 ];
 
 const TextField = styled.input`
-  height: 32px;
-  width: 200px;
-  border-radius: 3px;
-  border-top-left-radius: 5px;
-  border-bottom-left-radius: 5px;
-  border-top-right-radius: 0;
-  border-bottom-right-radius: 0;
-  border: 1px solid #e5e5e5;
-  padding: 0 32px 0 16px;
-  &:hover {
-    cursor: pointer;
-  }
+	height: 32px;
+	width: 200px;
+	border-radius: 3px;
+	border-top-left-radius: 5px;
+	border-bottom-left-radius: 5px;
+	border-top-right-radius: 0;
+	border-bottom-right-radius: 0;
+	border: 1px solid #e5e5e5;
+	padding: 0 32px 0 16px;
+	&:hover {
+		cursor: pointer;
+	}
 `;
 
 
 const ClearButton = styled.button`
-  border-top-left-radius: 0;
-  border-bottom-left-radius: 0;
-  border-top-right-radius: 5px;
-  border-bottom-right-radius: 5px;
-  height: 34px;
-  width: 32px;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	border-top-left-radius: 0;
+	border-bottom-left-radius: 0;
+	border-top-right-radius: 5px;
+	border-bottom-right-radius: 5px;
+	height: 34px;
+	width: 32px;
+	text-align: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 `;
 
 
 const FilterComponent = ({ filterText, onFilter, onClear }) => (
-  <>
-    <TextField
-      id="search"
-      type="text"
-      placeholder="Filter By Name"
-      aria-label="Search Input"
-      value={filterText}
-      onChange={onFilter}
-    />
+	<>
+		<TextField
+			id="search"
+			type="text"
+			placeholder="Filter By Name"
+			aria-label="Search Input"
+			value={filterText}
+			onChange={onFilter}
+		/>
 
-    <ClearButton type="button" onClick={onClear}>
-      X
-    </ClearButton>
-  </>
+		<ClearButton type="button" onClick={onClear}>
+			X
+		</ClearButton>
+	</>
 );
 
   export const Filtering = ({data}) => {
-    console.log(data)
+    const router = useRouter()
+
     const [filterText, setFilterText] = useState('');
     const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
     const filteredItems = data.filter( 
@@ -94,8 +96,8 @@ const FilterComponent = ({ filterText, onFilter, onClear }) => (
             String(item.root_span_host).includes(searchTerm) ||
             item.root_span_http_method.toLowerCase().includes(searchTerm) ||
             item.root_span_endpoint.toLowerCase().includes(searchTerm)  ||
-            String(item.trace_latency).includes(searchTerm)) ||
-            String(item.contains_errors).includes(searchTerm)
+            String(item.trace_latency).includes(searchTerm) ||
+            String(item.contains_errors).includes(searchTerm))
       }
     );
   
@@ -112,6 +114,12 @@ const FilterComponent = ({ filterText, onFilter, onClear }) => (
       )
     }
 
+    const handleRowClick = (e) => {
+      const traceId = e.trace_id
+      const href = `http://localhost:3000/traces/${traceId}`
+      router.push(href)
+    }
+
     return (
       <>
         <DataTable
@@ -122,6 +130,7 @@ const FilterComponent = ({ filterText, onFilter, onClear }) => (
             subHeader
             subHeaderComponent={returnFilteredComponent()}
             dense
+            onRowClicked={handleRowClick}
         />
       </>
     );
