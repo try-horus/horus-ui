a39f61f1be5c77cffe918c7e4434b6558ac24106
richwynmorris <rwmorris.bjj@gmail.com>
2021-11-21 13:53:18 +0000

    added merge files from main

--- client/components/pageHeader.jsx
+++ client/components/pageHeader.jsx
@@ -1,19 +1,11 @@
 
 function Header() {
   return (
-<<<<<<< HEAD
     <header className="p-8 bg-horusBlue text-white flex items-center" >
         <img className="rounded-lg w-24 ml-5" src="/horus_logo.png"/>
         <div className="w-full"></div>
         <p className="font-head">Documentation</p>
         <img className="rounded-lg w-12 ml-5" src="/github_logo.png"></img>
-=======
-    <header className="py-8 bg-blue-800 text-white ">
-      <div className="container flex mx-auto items-center justify-start">
-        <img className="rounded-lg w-24 ml-5" src="/horus-eye.png"/>
-        <h1 className="text-2xl px-6"> Horus.io</h1>
-      </div>
->>>>>>> main
     </header>
   )
 }
